21f2533a15b1b3ee74d752db13dc7c48ec6a67e8
webGrandpa <goga.khilo@gmail.com>
2025-09-10 11:01:02 +0400

    final version for exam

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -6,20 +6,6 @@ import closeicon from '../assets/closeicon.svg'
 import sendicon from '../assets/sendicon.svg'
 
 
-const CloseIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x">
-    <path d="M18 6 6 18"></path>
-    <path d="m6 6 12 12"></path>
-  </svg>
-);
-
-const SendIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-send-horizontal">
-    <path d="m3 3 3 9-3 9 19-9Z"></path>
-    <path d="M22 12H6"></path>
-  </svg>
-);
-
 
 const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
